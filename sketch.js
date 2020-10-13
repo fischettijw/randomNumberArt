e0a7d5295a347711f8ed37f28024ff96aa670e1b
Joseph Fischetti <fischettijw@gmail.com>
2020-10-13 17:40:23 -0400

    added frameRate(1/3)

--- sketch.js
+++ sketch.js
@@ -1,17 +1,17 @@
 function setup() {
     createCanvas(500, 500);
+    frameRate(1 / 3);
 }
 
 function draw() {
-    background(0);
+    background('black');
     generatePoints(width * height);
 }
 
 function generatePoints(n) {
     let x;
     let y;
-    let c = ['red'];
-    c = ['red', 'green', 'blue', 'cyan', 'yellow', 'magenta', 'white'];
+    let c = ['red', 'green', 'blue', 'cyan', 'yellow', 'magenta', 'white'];
     strokeWeight(1);
     for (i = 0; i < n; i++) {
         x = floor(random(0, width));
